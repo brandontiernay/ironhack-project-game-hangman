eb2963cede3806797723a256e67d9be5a58c1afe
Brandon Tiernay <btiernay@Brandons-MacBook-Air.local>
2023-01-23 17:24:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Words used in the game
+// const wordBank = [
+//     "flexbox",
+//     "algorithm",
+//     "conditionals",
+//     "interpolation",
+//     "concatenation", 
+// ]
+
+const word = "flexbox"
+
