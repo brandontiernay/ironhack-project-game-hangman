c4a28aff81a82ddf96753a33e03a1a0919564ca1
Brandon Tiernay <btiernay@Brandons-MacBook-Air.local>
2023-01-25 12:55:44 -0500

    Update script
    This second commit adds further functionality to the game by adding new variables, functions, and event listeners. Additional update to the index and styles files in the form of
    
    - <div id=visible-word></div> in the index file
    - #keyboard button:disabled in the css file
    -#visible-word{} in the css file

--- script.js
+++ script.js
@@ -7,34 +7,61 @@
 //     "concatenation", 
 // ]
 
-const word = "flexbox"
+
+const numberOfErrors = 0;
+const secretString = "flexbox";
+let visibleString = secretString.split('').fill('_').join('');
+
+const updateVisibleWord = (newWord) => {
+    const visibleWord = document.querySelector('#visible-word');
+    visibleWord.innerHTML = newWord;
+};
 
 // 'DOMContentLoaded' event is to make sure that all HTML elements are loaded in the DOM before executing callback.
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed'); // Put console in place to make sure it works before I continued working.
 
-    initializeKeyboard(); // Queries the document to find all the buttons and attempt to attach and eventListener to each.
+    initializeButtons(); // Queries the document to find all the buttons and attempt to attach and eventListener to each.
+    updateVisibleWord(visibleString);
 
   });
 
-  // Should make keyboard interactive by selecting all keys and adding eventListeners.
-  const initializeKeyboard = () => {
-   const buttons = document.querySelectorAll('#keyboard button'); // querySelectorAll functions kinda like CSS selectors in JS?
-   console.log(buttons);
+  // Should make buttons interactive by selecting all keys and adding eventListeners.
+  const initializeButtons = () => {
+    const newGameButton = document.querySelector('#new-game-button button');
+    newGameButton.addEventListener('click', (event) => {
+        resetGame();
+    });
+
+    const keyboardButtons = document.querySelectorAll('#keyboard button'); // querySelectorAll functions kinda like CSS selectors in JS?
+   console.log(keyboardButtons);
 
    // Element (button) is the button we do something to. Index is the position while it loops. Array is the entire buttons variable array.
-   buttons.forEach( (button, index, array) => {
+   keyboardButtons.forEach( (button, index, array) => {
+
 
     button.addEventListener('click', (event) => {
-
-        const letter = //????
+        const letter = button.innerHTML;
         handleLetterClick(letter);
+        button.disabled = true; // Disable pressed keys
     });
 
    });
 
   };
 
-  const handleLetterClick = (letter) => {
+  const handleCorrectLetter = (letter) => {
 
+  }
+
+  const handleWrongLetter = () => {
+
+  }
+
+  const handleLetterClick = (letter) => {
+    // if statements
+  }
+
+  const resetGame = () => {
+    location.reload();
   }
