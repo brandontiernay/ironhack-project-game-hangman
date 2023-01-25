7cdbc1d95138ef2f5af1d66771cabc21afec01d7
Brandon Tiernay <btiernay@Brandons-MacBook-Air.local>
2023-01-25 16:48:01 -0500

    Third commit
    This third commit makes some changes to the previous code on the script file and focused on the handleLetterClick and handleWrongLetter functions

--- script.js
+++ script.js
@@ -11,18 +11,19 @@
 const numberOfErrors = 0;
 const secretString = "flexbox";
 let visibleString = secretString.split('').fill('_').join('');
+const visibleWord = document.querySelector('#visible-word');
 
-const updateVisibleWord = (newWord) => {
-    const visibleWord = document.querySelector('#visible-word');
-    visibleWord.innerHTML = newWord;
-};
+// const updateVisibleWord = (newWord) => {
+//     const visibleWord = document.querySelector('#visible-word');
+//     visibleWord.innerHTML = newWord;
+// };
 
 // 'DOMContentLoaded' event is to make sure that all HTML elements are loaded in the DOM before executing callback.
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed'); // Put console in place to make sure it works before I continued working.
 
     initializeButtons(); // Queries the document to find all the buttons and attempt to attach and eventListener to each.
-    updateVisibleWord(visibleString);
+    // updateVisibleWord(visibleString);
 
   });
 
@@ -41,8 +42,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
     button.addEventListener('click', (event) => {
-        const letter = button.innerHTML;
+        // const letter = button.innerHTML;
+        console.log(event);
+        // Text from inside element
+        let letter = event.target.innerHTML;
+        console.log(event.target.innerHTML);
         handleLetterClick(letter);
+        handleWrongLetter(letter);
         button.disabled = true; // Disable pressed keys
     });
 
@@ -54,12 +60,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   }
 
-  const handleWrongLetter = () => {
-
+  let count = 0;
+  const handleWrongLetter = (letter) => {
+    for (let i = 0; i < secretString.length; i++) {
+        console.log(secretString[i]);
+        console.log(letter);
+        if (letter !== secretString[i] && count === 0) {
+           let platform = document.querySelector("#platform");
+           let head = document.querySelector("#head");
+           platform.style.display = "none";
+           head.style.visibility = "visible";
+           head.style.height = "30vh";
+        } else if (letter !== secretString[i] && count === 1) {
+            let platform = document.querySelector("#platform");
+            let head = document.querySelector("#head");
+            let torso = document.querySelector("#torso");
+            head.style.visibility = "hidden";
+            platform.style.display = "none";
+            torso.style.visibility = "visible";
+            torso.style.height = "30vh";
+            console.log("hey");
+         }
+    }
+    count++;
+    console.log(count);
   }
 
   const handleLetterClick = (letter) => {
-    // if statements
+    const letterElement = document.createElement("p");
+    letterElement.innerHTML = letter;
+    visibleWord.appendChild(letterElement);
   }
 
   const resetGame = () => {
